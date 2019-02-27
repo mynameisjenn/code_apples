5526d58d195e6ec6ba964311342f6062212c5b5d
mynameisjenn <jenniferlynnhatton@gmail.com>
2019-02-27 17:40:42 -0600

    added containers and partial for comments view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+
+	# http_basic_authenticate_with name: "orange", password: "banana", only: :destroy
 	
 	def create
 		@article = Article.find(params[:article_id])
