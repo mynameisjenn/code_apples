3164e328ce35163aa140a8cb96be9b7e91cf1e1d
mynameisjenn <jenniferlynnhatton@gmail.com>
2019-02-26 20:15:25 -0600

    added update and destroy methods, created partial for form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,12 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
-	
 	def new
 		@article = Article.new
 	end
 
-	def
-		@article = Article.find(params[:id]) 
+	def edit
+  		@article = Article.find(params[:id])
 	end
 
 	def create
@@ -27,6 +26,22 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end 
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
 
 	private
   		def article_params
