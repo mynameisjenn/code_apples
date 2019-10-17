a5829453988c6112c1014b93c7e84ce32ead3f0f
mynameisjenn <jenniferlynnhatton@gmail.com>
2019-10-16 23:54:35 -0500

    added about and contact page, added minor styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 # STEP 1: A ROUTE triggers a controller action
 # verb "/urls" => "namespace/controllers#action"
-  get 'welcome/index'
+  get 'welcome/index' => 'welcome#index'
+  get '/about' => 'welcome#about'
+  get '/contact' => 'welcome#contact'
 
   resources :articles do 
   	resources :comments
   end
   root 'welcome#index'
+
 end
